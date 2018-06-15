bac19098700c3652c0bad5ede75eb088c35bee23
ChuckMaurice34 <arpajouguillaume@free.fr>
2018-06-15 15:52:50 +0200

    Le3emeservice : 4- base.html.twig + index.html.twig + routes.yaml + MainController.php + divers liens dans navbar, footer et body

---
+++ src/Controller/MainController.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Controller;
+
+/* src/Controler/MainControler.php */
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class MainController extends Controller
+{
+	/**
+	* @Route("/index", name="index")
+	*/
+	public function index()
+	{
+		return $this->render('index.html.twig');
+	}
+}
